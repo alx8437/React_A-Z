d97ee00e7a747cff9981a010387f6c1f5d790cb5
Aleksandr Zelenskiy <dezion777@gmail.com>
2021-10-15 09:54:16 +0300

    input controlled state

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, {useState} from "react";
 function App() {
 
     const [like, setLike] = useState(7);
+    const [value, setValue] = useState('Text in input');
 
     function increment() {
         setLike(like + 1);
@@ -15,6 +16,12 @@ function App() {
     return (
         <div className="App">
             <h1>{like}</h1>
+            <h1>{value}</h1>
+            <input
+                type="text"
+                value={value}
+                onChange={event => setValue(event.currentTarget.value)}
+            />
             <button onClick={increment}>Increment</button>
             <button onClick={decrement}>Decrement</button>
         </div>
