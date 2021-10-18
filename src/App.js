3ac652260c96a4900416a81c0819dbe857eecad0
Aleksandr Zelenskiy <dezion777@gmail.com>
2021-10-18 19:29:32 +0300

    create func and class component

--- src/App.js
+++ src/App.js
@@ -1,29 +1,12 @@
-import React, {useState} from "react";
+import React from "react";
+import Counter from "./Components/Counter";
+import CounterClass from "./Components/CounterClass";
 
 function App() {
-
-    const [like, setLike] = useState(7);
-    const [value, setValue] = useState('Text in input');
-
-    function increment() {
-        setLike(like + 1);
-    }
-
-    function decrement() {
-        setLike(like - 1);
-    }
-
     return (
         <div className="App">
-            <h1>{like}</h1>
-            <h1>{value}</h1>
-            <input
-                type="text"
-                value={value}
-                onChange={event => setValue(event.currentTarget.value)}
-            />
-            <button onClick={increment}>Increment</button>
-            <button onClick={decrement}>Decrement</button>
+            <Counter />
+            <CounterClass />
         </div>
     );
 }
